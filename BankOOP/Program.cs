016c642b095bd04a00fd915abddb557b269aac7b
aiden lundeen <aiden.lundeen@student.centralia.edu>
2020-11-29 22:59:58 -0800

    why must I need a message

---
+++ BankOOP/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace BankOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            while (running == true)
+            {
+                string init;
+                char outit;
+                bool loggedin = false;
+                bool userincapable = true;
+                Console.WriteLine("Welcome to very safe and secure bank of NotaScamBanking.");
+                while (userincapable)
+                {
+                    Console.WriteLine("(L)ogin  (Q)uit");
+                    init = Console.ReadLine();
+                    init.ToLower();
+                    if (!char.TryParse(init, out outit))
+                    {
+                        Console.WriteLine("Please enter a valid input (L, or Q).");
+                    }
+                    switch (outit)
+                    {
+                        case 'l':
+                            if (false)
+                            {
+                                loggedin = true;
+                            }
+                            break;
+                        case 'q':
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter a valid input (L, or Q).");
+                            break;
+                    }
+                }
+
+                while (loggedin)
+                {
+                    Console.WriteLine("(W)ithdraw   (D)eposit   (B)alance   (L)ogout");
+                    init = Console.ReadLine();
+                    init.ToLower();
+                    if (!char.TryParse(init, out outit))
+                    {
+                        Console.WriteLine("Please enter a valid input (W, D, B, or L).");
+                    }
+                    switch (outit)
+                    {
+                        case 'w':
+
+                            break;
+                        case 'd':
+
+                            break;
+                        case 'b':
+
+                            break;
+                        case 'l':
+                            loggedin = false;
+                            break;
+                        default:
+                            Console.WriteLine("Enter a valid input (W, D, B, or L).");
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BankOOP/The_Bank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankOOP
+{
+    class The_Bank
+    {
+        private bool loggedIn;
+        private string name;
+        private string username;
+        private string password;
+        private decimal balance;
+        The_Bank(string _name, string user, string pass_safe, decimal initialBal)
+        {
+            name = _name;
+            username = user;
+            password = pass_safe;
+            balance = initialBal;
+        }
+        public string getName()
+        {
+            if (loggedIn == true)
+            {
+                return name;
+            }
+            else return "";
+        }
+        public string getUsername()
+        {
+            if (loggedIn == true)
+            {
+                return username;
+            }
+            else return "";
+        }
+        public decimal getBalance()
+        {
+            if (loggedIn == true)
+            {
+                return balance;
+            }
+            else return 0m;
+            
+        }
+        public bool verifylogin(string inputUsername, string inputPassword)
+        {
+            if (inputUsername == username)
+            {
+                if (inputPassword == password)
+                {
+                    loggedIn = true;
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
